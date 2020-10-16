eae934559ea37e74c4b6974c9521727dd0de75a0
roozbehR <61944934+roozbehR@users.noreply.github.com>
2020-10-16 15:04:10 -0400

    Added middlewares and routes which contain middlware and routes files

--- index.js
+++ index.js
@@ -1,25 +1,28 @@
 const express = require('express');
 const path = require('path');
 const { getMaxListeners } = require('process');
+const logger = require('./middleware/logger');
 const app = express();
 
-const members = [
-    {
-        "id": 1,
-        "name": "Jhon Doe",
-        "email": "john@gmail.com",
-        "status": "acitive"
-    }
-];
-app.get('/api/members', (req, res) => {
-    res.json(members);
-});
+//init middleware
+// app.use(logger);
+
+
+
+
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
 
-
+//Body parser Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: false}));
 //Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 app.listen(PORT, () => console.log(`server started on port ${PORT}`));
 
+//Members API routes
+app.use('/api/members', require('./routes/api/members'));
